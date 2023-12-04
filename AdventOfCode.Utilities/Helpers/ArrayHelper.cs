e4cbb06e69098e698d88c4626b9e126913bdf2d5
Sean Edwards <hello@seanedwards.dev>
2023-12-04 14:03:07 +0000

    Solution for day 3

---
+++ AdventOfCode.Utilities/Helpers/ArrayHelper.cs
@@ -0,0 +1,69 @@
+﻿namespace AdventOfCode.Utilities.Helpers;
+
+public static class ArrayHelper
+{
+    public static bool IsValidCoordinate<T>(int x, int y, T[][] array)
+    {
+        return !(x < 0 || y < 0 || x > array.Length - 1 || y > array[0].Length - 1);
+    }
+
+    public static void ArrayPrinter<T>(T[][] array)
+    {
+        foreach (var row in array)
+        {
+            foreach (var item in row)
+            {
+                Console.Write(item);
+            }
+
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+    }
+
+    public static void ArrayPrinter<T>(T[,] array)
+    {
+        var row = array.GetLength(0);
+        var col = array.GetLength(1);
+
+        for (var i = 0; i < row; i++)
+        {
+            for (var j = 0; j < col; j++)
+            {
+                Console.Write(array[i, j]);
+            }
+
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+    }
+
+    public static bool ArraysAreTheSame<T>(T[][] arr1, T[][] arr2) where T : IComparable<T>
+    {
+        var row = arr1.Length;
+        var col = arr1[0].Length;
+
+        for (var i = 0; i < row; i++)
+        {
+            for (var j = 0; j < col; j++)
+            {
+                if (arr1[i][j].CompareTo(arr2[i][j]) != 0)
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static IEnumerable<(int, int)> GetSurroundingValues<T>(int x, int y, T[][] array)
+    {
+        var surroundingCoordinates = new List<(int X, int Y)>
+        {
+            (x - 1, y - 1), (x - 1, y), (x - 1, y + 1), (x, y - 1), (x, y + 1), (x + 1, y - 1), (x + 1, y), (x + 1, y + 1)
+        };
+
+        return surroundingCoordinates.Where(coordinate => IsValidCoordinate(coordinate.X, coordinate.Y, array)).Select(coordinate => (coordinate.X, coordinate.Y));
+    }
+}
---
+++ AdventOfCode/Days/Day03.cs
@@ -0,0 +1,102 @@
+﻿using System.Text;
+using AdventOfCode.Utilities.Helpers;
+
+namespace AdventOfCode.Days;
+
+internal class Day03 : BaseDay
+{
+    private readonly char[][] _input;
+
+    public Day03()
+    {
+        _input = File.ReadAllText(InputFilePath).Trim().Split(Environment.NewLine).Select(x => x.ToCharArray()).ToArray();
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        var partNumbers = 0;
+
+        for (var i = 0; i < _input.Length; i++)
+        {
+            var line = _input[i];
+            var number = new StringBuilder();
+            var numberTouchingSymbol = false;
+
+            for (int j = 0; j < line.Length; j++)
+            {
+                if (char.IsDigit(line[j]))
+                {
+                    number.Append(line[j]);
+
+                    foreach (var value in ArrayHelper.GetSurroundingValues(i, j, _input))
+                    {
+                        if (_input[value.Item1][value.Item2] != '.' && !char.IsDigit(_input[value.Item1][value.Item2]))
+                        {
+                            numberTouchingSymbol = true;
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    if (numberTouchingSymbol)
+                        partNumbers += int.Parse(number.ToString());
+
+                    if (number.Length > 0)
+                        number.Clear();
+
+                    numberTouchingSymbol = false;
+                }
+            }
+        }
+
+        return new(partNumbers.ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        var gears = new Dictionary<(int, int), List<int>>();
+        for (var i = 0; i < _input.Length; i++)
+        {
+            var line = _input[i];
+            var number = new StringBuilder();
+            var numberTouchingGear = false;
+            var gearCoordinate = (0, 0);
+
+            for (int j = 0; j < line.Length; j++)
+            {
+                if (char.IsDigit(line[j]))
+                {
+                    number.Append(line[j]);
+
+                    foreach (var value in ArrayHelper.GetSurroundingValues(i, j, _input))
+                    {
+                        if (_input[value.Item1][value.Item2] == '*')
+                        {
+                            numberTouchingGear = true;
+
+                            if (!gears.ContainsKey(value))
+                                gears.Add(value, []);
+
+                            gearCoordinate = value;
+
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    if (numberTouchingGear)
+                        gears[gearCoordinate].Add(int.Parse(number.ToString()));
+
+                    if (number.Length > 0)
+                        number.Clear();
+
+                    numberTouchingGear = false;
+                }
+            }
+        }
+
+        return new(gears.Where(x => x.Value.Count == 2).Sum(x => x.Value[0] * x.Value[1]).ToString());
+    }
+}
