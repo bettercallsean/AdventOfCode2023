50ae6a19bc7fff95738b1646986998a4b75a36e6
Sean Edwards <hello@seanedwards.dev>
2023-12-07 13:54:47 +0000

    Create method for ordering hands

--- AdventOfCode/Days/Day07.cs
+++ AdventOfCode/Days/Day07.cs
@@ -45,47 +45,7 @@ public class Day07 : BaseDay
             orderedHands.Add((hand.Key, hand.Value, _handPriorities[handCount]));
         }
 
-        var cards = new List<int>();
-        foreach (var item in orderedHands.OrderBy(x => x.Item3).GroupBy(x => x.Item3))
-        {
-            var groupedHands = item.ToList();
-
-            if (groupedHands.Count == 1)
-            {
-                cards.AddRange(groupedHands.Select(x => x.Item2));
-                continue;
-            }
-
-            for (var i = 0; i < groupedHands.Count - 1; i++)
-            {
-                var swapped = false;
-
-                for (var j = 0; j < groupedHands.Count - 1; j++)
-                {
-                    var ordered = false;
-
-                    for (var k = 0; k < 5; k++)
-                    {
-                        if (groupedHands[j].Item1[k] != groupedHands[j + 1].Item1[k])
-                        {
-                            ordered = cardPriorities[groupedHands[j].Item1[k]] < cardPriorities[groupedHands[j + 1].Item1[k]];
-                            break;
-                        }
-                    }
-
-                    if (!ordered)
-                    {
-                        (groupedHands[j], groupedHands[j + 1]) = (groupedHands[j + 1], groupedHands[j]);
-                        swapped = true;
-                    }
-                }
-
-                if (!swapped)
-                    break;
-            }
-
-            cards.AddRange(groupedHands.Select(x => x.Item2));
-        }
+        var cards = OrderCards(orderedHands.OrderBy(x => x.Item3).GroupBy(x => x.Item3).ToList(), cardPriorities);
 
         var score = 0;
         for (var i = 0; i < cards.Count; i++)
@@ -119,21 +79,35 @@ public class Day07 : BaseDay
 
         foreach (var hand in _input)
         {
-            var foo = hand.Key.GroupBy(x => x).ToDictionary(x => x.First(), x => x.Count());
+            var cardCounts = hand.Key.GroupBy(x => x).ToDictionary(x => x.First(), x => x.Count());
 
-            if (foo.TryGetValue('J', out int value) && foo['J'] != 5)
+            if (cardCounts.TryGetValue('J', out int value) && cardCounts['J'] != 5)
             {
-                var key = foo.Where(x => x.Key != 'J').MaxBy(kvp => kvp.Value).Key;
-                foo[key] += value;
-                foo.Remove('J');
+                var key = cardCounts.Where(x => x.Key != 'J').MaxBy(kvp => kvp.Value).Key;
+                cardCounts[key] += value;
+                cardCounts.Remove('J');
             }
 
-            var handCount = string.Join(string.Empty, foo.OrderByDescending(x => x.Value).Select(x => x.Value));
+            var handCount = string.Join(string.Empty, cardCounts.OrderByDescending(x => x.Value).Select(x => x.Value));
             orderedHands.Add((hand.Key, hand.Value, _handPriorities[handCount]));
         }
 
+        var cards = OrderCards(orderedHands.OrderBy(x => x.Item3).GroupBy(x => x.Item3).ToList(), cardPriorities);
+
+        var score = 0;
+        for (var i = 0; i < cards.Count; i++)
+        {
+            score += cards[i] * (i + 1);
+        }
+
+        return new(score.ToString());
+    }
+
+    private static List<int> OrderCards(List<IGrouping<int, (string, int, int)>> orderedHands, Dictionary<char, int> cardPriorities)
+    {
         var cards = new List<int>();
-        foreach (var item in orderedHands.OrderBy(x => x.Item3).GroupBy(x => x.Item3))
+
+        foreach (var item in orderedHands)
         {
             var groupedHands = item.ToList();
 
@@ -174,12 +148,6 @@ public class Day07 : BaseDay
             cards.AddRange(groupedHands.Select(x => x.Item2));
         }
 
-        var score = 0;
-        for (var i = 0; i < cards.Count; i++)
-        {
-            score += cards[i] * (i + 1);
-        }
-
-        return new(score.ToString());
+        return cards;
     }
 }
